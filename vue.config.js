8f6e9d8a01d1df23558026302798895b745a0703
Ibmk12 <75578305+Ibmk12@users.noreply.github.com>
2025-09-27 00:59:50 +0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   devServer: {
-    port: 3000, // You can change this to any port number you prefer
+    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: process.env.VUE_APP_API_URL || 'http://localhost:8080',
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-}
+        secure: false,
+      },
+    },
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+};
