88d2cc278d3ffc6d51dd8b6f226b55f54caac798
Ibmk12 <75578305+Ibmk12@users.noreply.github.com>
2025-09-27 01:54:25 +0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,16 @@
+const apiUrl = process.env.VUE_APP_API_URL || 'https://karrardelivery.onrender.com';
+
 module.exports = {
   devServer: {
     port: 3000, // You can change this to any port number you prefer
     proxy: {
       '/api': {
-        target: 'https://karrardelivery.onrender.com',
+        target: apiUrl,
         changeOrigin: true,
         secure: false
       }
     }
-  }
+  },
+  // Disable linting during build
+  lintOnSave: false
 }
