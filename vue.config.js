8f998f6c60510f7c8bdfb0bc04c0b63249414ec2
Ibmk12 <75578305+Ibmk12@users.noreply.github.com>
2025-09-27 01:07:29 +0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  lintOnSave: false, // disable ESLint checks on save/build
   devServer: {
     port: 3000,
     proxy: {
@@ -6,8 +7,7 @@ module.exports = {
         target: process.env.VUE_APP_API_URL || 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
-      },
-    },
-  },
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-};
+      }
+    }
+  }
+}
